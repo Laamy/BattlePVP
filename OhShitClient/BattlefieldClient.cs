ef209a20f04e2e558b88e7f068e3520d3d12d7fe
YeemiRouth <yeemirouth@gmail.com>
2023-12-31 05:45:24 +1100

    Add project files.

---
+++ OhShitClient/BattlefieldClient.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using static User32;
+
+class BattlefieldClient
+{
+    // information about battlefield like its process ID
+
+    /// <summary>
+    /// Unsigned int (To store the battlefield handle in for winhooks)
+    /// </summary>
+    public static uint GameId;
+
+    /// <summary>
+    /// Int Pointer (32bit number) (To store the battlefiel window handle in for winhooks, again)
+    /// </summary>
+    public static IntPtr WinHandle;
+
+    /// <summary>
+    /// Name of the game
+    /// </summary>
+    public static string GameName = "BF2042";
+    public static string GameTitle = "Battlefield™ 2042";
+
+    public static void OpenGame()
+    {
+        // locate rust in the memory
+        Process game = Process.GetProcessesByName(GameName)[0]; // oops title is Battlefield™ 2042
+
+        GameId = (uint)game.Id; // game id
+        WinHandle = game.MainWindowHandle; // window handle
+    }
+
+    public static ProcessRectangle GetGameRect()
+    {
+        // get window rect into "rect" variable
+        ProcessRectangle rect;
+        User32.GetWindowRect(WinHandle, out rect);
+
+        // return window dimensions
+        return rect;
+    }
+
+    public static IntPtr IsGameFocusedInsert()
+    {
+        var sb = new StringBuilder(GameTitle.Length + 1);
+        GetWindowText(GetForegroundWindow(), sb, GameTitle.Length + 1);
+        if (sb.ToString() == GameTitle)
+            return (IntPtr)(-1);
+        return (IntPtr)(-2);
+    }
+
+    #region Structs
+    /// <summary>
+    /// Window Rectangle
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ProcessRectangle
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+        public ProcessRectangle(Point position, Point size) // this is most likely wrong
+        {
+            this.Left = position.X;
+            this.Top = position.X + size.X;
+            this.Right = position.Y;
+            this.Bottom = position.Y + size.Y;
+
+            // Left, Top, Right, Bottom
+            // X, X - X, Y, Y - Y
+
+            // Left, Top,
+            // Right, Bottom
+            // X, X - X,
+            // Y, Y - Y
+        }
+    }
+    #endregion
+}
---
+++ OhShitClient/UIBase/Overlay.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+using static BattlefieldClient;
+using static User32;
+using static Debug;
+
+class Overlay : Form
+{
+    public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+    public static WinEventDelegate overDel;
+
+    public Overlay()
+    {
+        Log("Initializing  UI..");
+
+        InitializeComponents();
+
+        Log("Initializing  Paint Hooks..");
+        Paint += OnUpdate;
+
+        Log("Initializing  Delegate..");
+
+        Focus(); // bring the overlay window into focus
+
+        // click through
+        int initStyle = GetWindowLong(Handle, -20);
+        SetWindowLong(Handle, -20, initStyle | 0x80000 | 0x20);
+
+        overDel = new WinEventDelegate(OnAdjust);
+
+        Log("Initializing  WinHooks..");
+
+        SetWinEventHook((uint)SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE, (uint)SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE, IntPtr.Zero, overDel, GameId, GetWindowThreadProcessId(WinHandle, IntPtr.Zero), (uint)SWEH_dwFlags.WINEVENT_OUTOFCONTEXT | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNPROCESS | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNTHREAD);
+        SetWinEventHook((uint)SWEH_Events.EVENT_SYSTEM_FOREGROUND, (uint)SWEH_Events.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, overDel, 0, 0, (uint)SWEH_dwFlags.WINEVENT_OUTOFCONTEXT | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNPROCESS | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNTHREAD);
+    }
+
+    public void OnUpdate(object sender, PaintEventArgs e)
+    {
+        Graphics g = e.Graphics;
+
+        int centerX = e.ClipRectangle.Width / 2;
+        int centerY = e.ClipRectangle.Height / 2;
+
+        Pen pen = new Pen(Color.Green, 2);
+
+        g.DrawLine(pen, centerX - 4, centerY, centerX + 4, centerY);
+
+        g.DrawLine(pen, centerX, centerY - 4, centerX, centerY + 4);
+    }
+
+    public void InitializeComponents()
+    {
+        // initialize winform here
+        TopMost = true; // not needed
+        TransparencyKey = BackColor;
+
+        FormBorderStyle = FormBorderStyle.None;
+
+        Text = GameTitle;
+
+        DoubleBuffered = true; // no tearing cuz this is gay
+    }
+
+    public void OnAdjust(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+    {
+        ProcessRectangle rect = GetGameRect();
+
+        int x = rect.Left; // wont do fullscreen yet
+        int y = rect.Top;
+        int width = rect.Right - rect.Left;
+        int height = rect.Bottom - rect.Top;
+
+        SetWindowPos(Handle, IsGameFocusedInsert(), x, y, width, height, 0x40); // might be working
+    }
+}
